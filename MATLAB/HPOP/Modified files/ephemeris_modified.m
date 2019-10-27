2f869dac9f73e7d25f2f063b612ab98ade5413b0
Carl Martin Krokeide <carlmk@stud.ntnu.no>
2019-10-27 13:37:11 +0100

    Added OrbitNTNU sun synchronous data

---
+++ MATLAB/HPOP/Modified files/ephemeris_modified.m
@@ -0,0 +1,8 @@
+
+function Eph = ephemeris_modified(Y0, N_Step, Step)
+
+options = rdpset('RelTol',1e-13,'AbsTol',1e-16);
+[t,yout] = radau(@Accel_modified,(0:Step:N_Step*Step),Y0,options);
+Eph(:,1) = t;
+Eph(:,2:7) = yout;
+
---
+++ MATLAB/orbitDeterminationOrbitNTNU.m
@@ -0,0 +1,30 @@
+% OrbitNTNU: orbit parameters based on MTI (P97-3)
+
+run earthParameters;
+
+T = 0; % 
+i = 97.4; % Deg
+O = 0;
+w = 0;
+
+rSat_a = rEarth + 609
+rSat_p = rEarth + 574
+
+aSat = 0.5 * (rSat_a + rSat_p)
+
+tSat_check = 2*(pi/sqrt(muEarth))*aSat^(3/2)
+
+eSat = 1 - (rSat_p / aSat)
+
+hNorm = sqrt(muEarth * aSat * ( 1 - eSat^2)) 
+
+
+r0PQW = positionVectorPQW( muEarth, hNorm, eSat, T );
+v0PQW = velocityVectorPQW( muEarth, hNorm, eSat, T );
+QmatPQWtoECI = transformPQWtoECI( i, O, w );
+r0ECI = QmatPQWtoECI * r0PQW
+v0ECI = QmatPQWtoECI * v0PQW
+
+rNorm = norm(r0ECI)
+vNorm = norm(v0ECI)
+
