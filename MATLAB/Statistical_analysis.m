c0cd871c27e38a294dd84b10c78b1b191cd4d6e9
Carl Martin Krokeide <carlmk@stud.ntnu.no>
2019-11-14 14:09:14 +0100

    prepared statistical moments measure

---
+++ MATLAB/Statistical_analysis.m
@@ -0,0 +1,28 @@
+x1;
+x2;
+
+
+%%
+
+
+mean1 = mean(x1)
+mean2 = mean(x2)
+
+%%
+
+
+var1 = var(x1)
+var2 = var(x2)
+
+%%
+
+skew1 = skewness(x1)
+skew2 = skewness(x2)
+
+%%
+
+kurt1 = kurtosis(x1)
+kurt2 = kurtosis(x2)
+
+
+
